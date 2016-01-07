76d6fef3f5b457fbd9457433f19bd9432d8a5549
gpuellestorres <gpuellestorres@gmail.com>
2016-01-07 11:47:07 -0300

    Inicio

---
+++ RentaMaq/Controllers/registrokmhmController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RentaMaq.DAL;
+using RentaMaq.Models;
+
+namespace RentaMaq.Controllers
+{
+    public class registrokmhmController : Controller
+    {
+        private Context db = new Context();
+        private int numeroPermiso = 12;
+
+        // GET: registrokmhm
+        public ActionResult Index(string inicio, string termino, string equipoID)
+        {
+            DateTime Inicio = DateTime.Now.AddMonths(-1);
+            DateTime Termino = DateTime.Now;
+
+            String EquipoID = "todos";
+
+            if (inicio != null || termino != null)
+            {
+                string[] inicioSeparado = inicio.Split('-');
+                string[] terminoSeparado = termino.Split('-');
+
+                Inicio = new DateTime(int.Parse(inicioSeparado[2]), int.Parse(inicioSeparado[1]), int.Parse(inicioSeparado[0]));
+                Termino = new DateTime(int.Parse(terminoSeparado[2]), int.Parse(terminoSeparado[1]), int.Parse(terminoSeparado[0]));
+            }
+
+            DateTime fin = Termino;
+            DateTime temp = fin;
+            while (temp.Day == Termino.Day)
+            {
+                fin = temp;
+                temp = temp.AddMinutes(1);
+            }
+
+            List<registrokmhm> lista = db.registrokmhms.Where(s => s.fecha >= Inicio && s.fecha <= fin).ToList();
+
+            if (equipoID != null && !equipoID.Equals("todos"))
+            {
+                EquipoID = equipoID;
+                int intEquipoID = int.Parse(EquipoID);
+                lista = lista.Where(s => s.equipoID == intEquipoID).OrderByDescending(s=>s.fecha).ToList();
+            }
+
+            ViewBag.Inicio = Inicio;
+            ViewBag.Termino = Termino;
+            ViewBag.EquipoID = EquipoID;
+
+            return View(lista);
+        }
+
+        // GET: registrokmhm/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registrokmhm registrokmhm = db.registrokmhms.Find(id);
+            if (registrokmhm == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registrokmhm);
+        }
+
+        // GET: registrokmhm/Create
+        public ActionResult Create()
+        {
+            if (Session["ID"] == null || !roles.tienePermiso(numeroPermiso, int.Parse(Session["ID"].ToString())))
+            {
+                return RedirectToAction("Index", "registrokmhm");
+            }
+            return View();
+        }
+
+        // POST: registrokmhm/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "registrokmhmID,equipoID,kilometraje,horometro,fecha")] registrokmhm registrokmhm,
+            FormCollection post)
+        {
+            if (Session["ID"] == null || !roles.tienePermiso(numeroPermiso, int.Parse(Session["ID"].ToString())))
+            {
+                return RedirectToAction("Index", "registrokmhm");
+            }
+            registrokmhm.fecha = Formateador.fechaFormatoGuardar(post["fecha"]);
+            db.registrokmhms.Add(registrokmhm);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: registrokmhm/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (Session["ID"] == null || !roles.tienePermiso(numeroPermiso, int.Parse(Session["ID"].ToString())))
+            {
+                return RedirectToAction("Index", "registrokmhm");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registrokmhm registrokmhm = db.registrokmhms.Find(id);
+            if (registrokmhm == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registrokmhm);
+        }
+
+        // POST: registrokmhm/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "registrokmhmID,equipoID,kilometraje,horometro,fecha")] registrokmhm registrokmhm, FormCollection post)
+        {
+            if (Session["ID"] == null || !roles.tienePermiso(numeroPermiso, int.Parse(Session["ID"].ToString())))
+            {
+                return RedirectToAction("Index", "registrokmhm");
+            }
+            registrokmhm.fecha = Formateador.fechaFormatoGuardar(post["fecha"]);
+            db.Entry(registrokmhm).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: registrokmhm/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (Session["ID"] == null || !roles.tienePermiso(numeroPermiso, int.Parse(Session["ID"].ToString())))
+            {
+                return RedirectToAction("Index", "registrokmhm");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            registrokmhm registrokmhm = db.registrokmhms.Find(id);
+            if (registrokmhm == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registrokmhm);
+        }
+
+        // POST: registrokmhm/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            if (Session["ID"] == null || !roles.tienePermiso(numeroPermiso, int.Parse(Session["ID"].ToString())))
+            {
+                return RedirectToAction("Index", "registrokmhm");
+            }
+            registrokmhm registrokmhm = db.registrokmhms.Find(id);
+            db.registrokmhms.Remove(registrokmhm);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RentaMaq/Models/registrokmhm.cs
@@ -0,0 +1,148 @@
+﻿using RentaMaq.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace RentaMaq.Models
+{
+    public class registrokmhm
+    {
+        public int registrokmhmID { get; set; }
+        [Display(Name = "Equipo")]
+        public int equipoID { get; set; }
+        [Display(Name = "Kilometraje")]
+        public int kilometraje { get; set; }
+        [Display(Name = "Horómetro")]
+        public int horometro { get; set; }
+        [Display(Name = "Fecha")]
+        public DateTime fecha { get; set; }
+
+        public static registrokmhm obtenerUltimo(int ID)
+        {
+            registrokmhm retorno = new registrokmhm();
+            SqlConnection con = conexion.crearConexion();
+            con.Open();
+
+            using (SqlCommand command = new SqlCommand("SELECT TOP 1 registrokmhmID FROM registrokmhm "
+                + "WHERE equipoID=@equipoID ORDER BY fecha DESC", con))
+            {
+                command.Parameters.Add("@equipoID", SqlDbType.Int).Value = ID;
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        retorno = new Context().registrokmhms.Find(int.Parse(reader[0].ToString()));
+                    }
+                }
+            }
+
+            con.Close();
+            return retorno;
+        }
+
+        public static void actualizarRegistroKmHm(int equipoID, DateTime fecha, int horometro, int kilometraje)
+        {
+            Context db = new Context();
+            db.registrokmhms.RemoveRange(db.registrokmhms.Where(s=>s.equipoID==equipoID && s.fecha==fecha));
+
+            db.SaveChanges();
+
+            registrokmhm nuevo = new registrokmhm();
+            nuevo.equipoID = equipoID;
+            nuevo.fecha=fecha;
+            nuevo.horometro = horometro;
+            nuevo.kilometraje = kilometraje;
+
+            db.registrokmhms.Add(nuevo);
+            db.SaveChanges();
+        }
+
+        public static void actualizarRegistroKmHm(registrokmhm nuevo)
+        {
+            actualizarRegistroKmHm(nuevo.equipoID, nuevo.fecha, nuevo.horometro, nuevo.kilometraje);
+        }
+
+        internal static registrokmhm obtenerUltimo(int equipoID, DateTime fin)
+        {
+            registrokmhm retorno = new registrokmhm();
+            SqlConnection con = conexion.crearConexion();
+            con.Open();
+
+            using (SqlCommand command = new SqlCommand("SELECT TOP 1 registrokmhmID FROM registrokmhm "
+                + "WHERE equipoID=@equipoID AND fecha<=@fecha ORDER BY fecha DESC", con))
+            {
+                command.Parameters.Add("@equipoID", SqlDbType.Int).Value = equipoID;
+                command.Parameters.Add("@fecha", SqlDbType.DateTime).Value = fin;
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        retorno = new Context().registrokmhms.Find(int.Parse(reader[0].ToString()));
+                    }
+                }
+            }
+
+            con.Close();
+            return retorno;
+        }
+
+        internal static registrokmhm obtenerEstimado(int equipoID, DateTime fecha)
+        {
+            registrokmhm anterior = obtenerUltimo(equipoID, fecha);
+            registrokmhm siguiente = obtenerSiguiente(equipoID, fecha);
+            registrokmhm masAnterior = new registrokmhm();
+
+            registrokmhm resultado = new registrokmhm();
+            resultado.equipoID = equipoID;
+            resultado.fecha = fecha;
+
+            if (siguiente.fecha.Equals(new registrokmhm().fecha))
+            {
+                masAnterior = obtenerUltimo(equipoID, fecha.AddMonths(-3));
+
+                TimeSpan diferencia = anterior.fecha - masAnterior.fecha;
+                int diferenciaHorometro = anterior.horometro - masAnterior.horometro;
+
+                resultado.horometro = (int)(masAnterior.horometro + (resultado.fecha - masAnterior.fecha).TotalDays
+                    * diferenciaHorometro / diferencia.TotalDays);
+            }
+            else
+            {
+                TimeSpan diferencia = siguiente.fecha - anterior.fecha;
+                int diferenciaHorometro = siguiente.horometro - anterior.horometro;
+
+                resultado.horometro = (int)(anterior.horometro + (resultado.fecha - anterior.fecha).TotalDays
+                    * diferenciaHorometro / diferencia.TotalDays);
+            }
+            return resultado;
+        }
+
+        private static registrokmhm obtenerSiguiente(int equipoID, DateTime fin)
+        {
+            registrokmhm retorno = new registrokmhm();
+            SqlConnection con = conexion.crearConexion();
+            con.Open();
+
+            using (SqlCommand command = new SqlCommand("SELECT TOP 1 registrokmhmID FROM registrokmhm "
+                + "WHERE equipoID=@equipoID AND fecha>@fecha ORDER BY fecha ASC", con))
+            {
+                command.Parameters.Add("@equipoID", SqlDbType.Int).Value = equipoID;
+                command.Parameters.Add("@fecha", SqlDbType.DateTime).Value = fin;
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        retorno = new Context().registrokmhms.Find(int.Parse(reader[0].ToString()));
+                    }
+                }
+            }
+
+            con.Close();
+            return retorno;
+        }
+    }
+}
